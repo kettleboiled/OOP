204f4f5b410ca164b777e63ca6baf6e447ed7e1e
kettleboiled <kettleboiled@users.noreply.github.com>
2025-11-03 13:08:05 +0000

    Deploying to gh-pages from @ kettleboiled/OOP@b033a17fac6328286a9a1329b02823b0137a707f 🚀

---
+++ Task_1_2_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.ryzhneva","l":"HashTable.Entry"},{"p":"ru.nsu.ryzhneva","l":"HashTable"},{"p":"ru.nsu.ryzhneva","l":"Main"}];updateSearchResults();
