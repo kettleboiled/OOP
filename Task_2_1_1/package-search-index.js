c1beb0cd47694a22893157daab1c7bbd485f3a3e
kettleboiled <kettleboiled@users.noreply.github.com>
2026-02-08 17:23:55 +0000

    Deploying to gh-pages from @ kettleboiled/OOP@14b3f0d4b2225e84e03d8af2e0010dad23a4516e 🚀

--- Task_2_1_1/package-search-index.js
+++ Task_2_1_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.ryzhneva"},{"l":"ru.nsu.ryzhneva.primalitytester"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.ryzhneva.primalitytester"}];updateSearchResults();
