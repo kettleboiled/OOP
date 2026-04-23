30ced133e10c7db8de7a75db669e5a4706a2b038
kettleboiled <kettleboiled@users.noreply.github.com>
2026-04-23 15:55:44 +0000

    Deploying to gh-pages from @ kettleboiled/OOP@5dc9ad5c4995da95ccc16503af52ac4c93445165 🚀

---
+++ Task_2_4_1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.ryzhneva"},{"l":"ru.nsu.ryzhneva.domain"},{"l":"ru.nsu.ryzhneva.results"}];updateSearchResults();
